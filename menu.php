a1b232800fe032e5718ce79c9c21ad66b0253612
javier <alumnsistemas@gmail.com>
2021-03-07 20:19:55 -0500

    modificación del meta

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="tienda virtual, paginas adaptables, paginas web responsive, paginas web para negocios, marketing digital, seo, diseño web"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2? family = Roboto: wght @ 100; 300; 400; 700 & display = swap " rel=" hoja de estilo" />
